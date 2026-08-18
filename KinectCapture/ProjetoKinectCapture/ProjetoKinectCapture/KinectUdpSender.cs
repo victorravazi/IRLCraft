a2abc05a2f6e53bc2f23c1ae6e80b595dcb23d70
Victor <victorrafaelravazi1234@gmail.com>
2026-08-18 15:23:40 -0300

    Mudanca para possibilitar a deteccao de duas pessoas e enviar os dados corretamente

--- KinectCapture/ProjetoKinectCapture/ProjetoKinectCapture/KinectUdpSender.cs
+++ KinectCapture/ProjetoKinectCapture/ProjetoKinectCapture/KinectUdpSender.cs
@@ -1,94 +1,71 @@
 ﻿using Microsoft.Kinect;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
 public class KinectUdpSender
 {
     private UdpClient client;
-
     private IPEndPoint endpoint;
 
-    public KinectUdpSender(
-        string ip,
-        int port)
+    public KinectUdpSender(string ip, int port)
     {
         client = new UdpClient();
-
-        endpoint =
-            new IPEndPoint(
-                IPAddress.Parse(ip),
-                port
-            );
+        endpoint = new IPEndPoint(IPAddress.Parse(ip), port);
     }
 
-    public void Send(Skeleton skeleton)
+    public void Send(List<Skeleton> skeletons)
     {
-        if (skeleton == null)
+        if (skeletons == null)
             return;
 
-        byte[] data = new byte[4 + (20 * 12)];
+        int personSize = 8 + 4 + (20 * 12);
+        int packetSize = 4 + (skeletons.Count * personSize);
+
+        byte[] data = new byte[packetSize];
 
         int offset = 0;
-
-        // Quantidade de joints
-        WriteInt(data, offset, 20);
-
+        WriteInt(data, offset, skeletons.Count);
         offset += 4;
 
-        for (int i = 0; i < 20; i++)
+        foreach (Skeleton skeleton in skeletons)
         {
-            JointType type =
-                (JointType)i;
-
-            Joint joint =
-                skeleton.Joints[type];
-
-            SkeletonPoint p =
-                joint.Position;
-
-            WriteFloat(
-                data,
-                offset,
-                p.X
-            );
-
+            WriteLong(data, offset, skeleton.TrackingId);
+            offset += 8;
+            WriteInt(data,offset, 20);
             offset += 4;
 
-            WriteFloat(
-                data,
-                offset,
-                p.Y
-            );
-
-            offset += 4;
-
-            WriteFloat(
-                data,
-                offset,
-                p.Z
-            );
-
-            offset += 4;
+            
+            for (int i = 0; i < 20; i++)
+            {
+                JointType type = (JointType)i;
+                Joint joint = skeleton.Joints[type];
+                SkeletonPoint p = joint.Position;
+                WriteFloat(data, offset, p.X);
+                offset += 4;
+                WriteFloat(data, offset, p.Y);
+                offset += 4;
+                WriteFloat(data, offset, p.Z);
+                offset += 4;
+            }
         }
 
-        client.Send(
-            data,
-            data.Length,
-            endpoint
-        );
+        client.Send(data, data.Length, endpoint);
     }
 
     private void WriteInt(byte[] data, int offset, int value)
-
-
-
     {
         data[offset] = (byte)(value & 0xFF);
         data[offset + 1] = (byte)((value >> 8) & 0xFF);
         data[offset + 2] = (byte)((value >> 16) & 0xFF);
         data[offset + 3] = (byte)((value >> 24) & 0xFF);
+    }
 
+    private void WriteLong(byte[] data, int offset, long value)
+    {
+        byte[] bytes = BitConverter.GetBytes(value);
+        Buffer.BlockCopy(bytes, 0, data, offset, 8);
     }
 
     private void WriteFloat(byte[] data, int offset, float value)
